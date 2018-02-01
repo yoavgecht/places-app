de015779ae3a3f6c91634b74b42692445dc6a205
Annapurna13 <yoavgecht@gmail.com>
2018-02-01 12:50:32 +0200

    package.json fixes

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if(port == 8080){
 
 
 
-app.use(express.static(__dirname + '/'))
+app.use(express.static(__dirname + '/build'))
 
 .get('*', (req, res) => {
     res.sendFile(__dirname + '/build/index.html');
