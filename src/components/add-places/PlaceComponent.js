a485828d7222be0119f652de2541e412169fbe2b
Annapurna13 <yoavgecht@gmail.com>
2018-02-23 23:06:55 +0200

    updates

--- src/components/add-places/PlaceComponent.js
+++ src/components/add-places/PlaceComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PlaceComponent = (props) => (
-        <li className="col-md-4" onClick={(e) => props.handleLocationClick(props.place.position)}>
+        <li className="col-md-4 destinations" onClick={(e) => props.handleLocationClick(props.place.position)}>
             <h4>{props.place.position.placeName}</h4>
             <img className="img-circle img-responsive img-location" src={props.place.position.placePhoto} alt={props.place.position.placeName}/>
         </li>
