03edc83ec50080c8e747cd3e890d0fe012721f4e
Annapurna13 <yoavgecht@gmail.com>
2018-03-05 16:01:44 +0200

    updates

--- src/components/add-places/PlaceComponent.js
+++ src/components/add-places/PlaceComponent.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const PlaceComponent = (props) => (
-        <li className="col-md-4 destinations" onMouseOver={(e) => props.handleLocationMouseOver(props.place.position)} onClick={(e) => props.handleLocationClick(props.place.position)}>
+        <li className="col-md-4 destinations" >
             <h4>{props.place.position.placeName}</h4>
-            <img className="img-responsive img-location" src={props.place.position.placePhoto} alt={props.place.position.placeName}/>
+            <img className="img-responsive img-location" src={props.place.position.placePhoto} alt={props.place.position.placeName} onMouseOver={(e) => props.handleLocationMouseOver(props.place.position)} onClick={(e) => props.handleLocationClick(props.place.position)}/>
         </li>
     );
 
