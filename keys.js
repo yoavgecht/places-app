f67913aa7fcbeb3cb23502bdff9f85274681217e
Annapurna13 <yoavgecht@gmail.com>
2018-03-09 09:18:51 +0200

    updates

---
+++ keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    db: {
+        user     : 'yoavgecht',
+        password : 'Annapurna13',
+        database : 'destinationsdb',
+        host     : 'destinationsdb.ceryqjmnlczp.eu-central-1.rds.amazonaws.com',   
+        port     : '3306'
+    }
+}
