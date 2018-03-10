8d5d660a0916a3ba8938eaa69ee36c951b33e25c
Annapurna13 <yoavgecht@gmail.com>
2018-03-10 10:05:32 +0200

    fixed untracked files

--- keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    db: {
-        user     : 'yoavgecht',
-        password : 'Annapurna13',
-        database : 'destinationsdb',
-        host     : 'destinationsdb.ceryqjmnlczp.eu-central-1.rds.amazonaws.com',   
-        port     : '3306'
-    }
-}
